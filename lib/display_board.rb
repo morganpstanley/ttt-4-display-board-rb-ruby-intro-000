1a9cd1f3555d9859c1c52d5e32506957b113dd07
Morgan Stanley <morganpstanley@gmail.com>
2019-06-11 22:08:51 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -3,7 +3,7 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 
 def board = ["   ","   ","   ","   ","   ","   ","   ","   ","   "]
-  
+
 def display_board(board)
 puts " #{board[0]}, | #{board[1]}, | #{board[2]} "}
 puts "-----------"
