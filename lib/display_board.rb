568ab8c98fe2af3a992edfc6d6c345f7c755334f
Morgan Stanley <morganpstanley@gmail.com>
2019-06-11 22:08:47 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,14 +1,15 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
-def display_board
-puts "   |   |   "
+
+def board = ["   ","   ","   ","   ","   ","   ","   ","   ","   "]
+  
+def display_board(board)
+puts " #{board[0]}, | #{board[1]}, | #{board[2]} "}
 puts "-----------"
-puts "   |   |   "
+puts " #{board[3]}, | #{board[4]}, | #{board[5]} "}
 puts "-----------"
-puts "   |   |   "
-end
+puts " #{board[6]}, | #{board[7]}, | #{board[8]} "}
 
 def display_board(board)
-  puts board
 end
